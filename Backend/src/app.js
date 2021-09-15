2e86fdb2a27b95565b55dfcd2a7f2fa9d23f55fd
Dilianisherrera <albertherrera30428@gmail.com>
2021-09-15 09:34:18 -0500

    mi-proyecto_01

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -32,3 +32,20 @@ app.use('/api/student', require('./routes/students'));
 app.use(express.static(path.join(__dirname, 'public')));;
 
 module.exports = app;
+// Middlewares
+app.use(cors()); //Cada petición que llegue podrá enviar y recibir datos
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+
+// Routes
+//app.use('/api/student', require('./routes/student'));
+app.use('/api/teacher', require('./routes/teachers'));
+app.use('/api/admin', require('./routes/admin'));
+app.use('/api/users', require('./routes/users'));
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));;
+
+module.exports = app;
